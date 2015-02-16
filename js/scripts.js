c2e0742ea41c00fab767cb3cef077640d65e4c9f
Ben Pritchard and Gabe Finch <ben.s.pritchard@gmail.com>
2015-02-16 15:58:53 -0800

    changes font

--- js/scripts.js
+++ js/scripts.js
@@ -1,25 +1,17 @@
+
+
 $(document).ready(function() {
   $("form#add-task").submit(function(event) {
     event.preventDefault();
 
     var taskName = $("#task").val();
-
     $(".to-do-list").append("<li>" + taskName + "</li>");
-
     $("#task").val("");
-
     $(".to-do-list").last().click(function(li) {
       $(li.target).appendTo(".done-list");
-
       $(".done-list").last().click(function(li) {
         $(li.target).remove();
       });
-      
     });
-
-
-
-
   });
-
 });
